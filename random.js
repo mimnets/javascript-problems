b197c3a2e53ada12232d0b1837468e63ead31bb2
mimnets <mdmonirulislammonir@gmail.com>
2020-07-15 00:44:36 +0600

    random number

---
+++ random.js
@@ -0,0 +1,11 @@
+var num = 2.8425;
+var result = Math.floor(num);
+var result2 = Math.ceil(num); 
+var result3 = Math.round(num);
+
+for(i = 0; i < 6; i++){
+    var randomNum = Math.random() * 6;
+    var output = Math.round(randomNum);
+    console.log(output);
+}
+
